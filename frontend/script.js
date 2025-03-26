81f63106e9dbabec9677d521c0efcbfdc9eab4db
Aase2007 <ase.luras@gmail.com>
2025-03-26 13:20:36 +0100

    oppdatering??

--- frontend/script.js
+++ frontend/script.js
@@ -2,9 +2,9 @@ let PROJECT_ID = 'hsjaawa5'
 let DATASET = 'production'
 let QUERY = encodeURIComponent(`*[_type == "varer"]{varenavn, pris, kategorinavn->{kategori}, bilde}`) //ber om alle varer med alle variabler
 let URL = `https://${PROJECT_ID}.api.sanity.io/v2021-10-21/data/query/${DATASET}?query=${QUERY}`
-let varer
+let varer //definerer varer her sånn at den finnes utafor hentVarer()
 
-function bildeURL(vareIndex) {
+function bildeURL(vareIndex) { //finner url-en til bildet
     let bildeListe = varer[vareIndex].bilde.asset._ref.split('-') //deler bildereferansen inn i deler slik at den er lesbar
     let imageURL = `https://cdn.sanity.io/images/${PROJECT_ID}/${DATASET}/${bildeListe[1]}-${bildeListe[2]}.${bildeListe[3]}` //lager URL for bilde
     return imageURL
@@ -13,7 +13,7 @@ function bildeURL(vareIndex) {
 async function hentVarer() {
     let response = await fetch(URL)
     let data = await response.json()
-    varer = data.result
+    varer = data.result //puter lista med objektene fra databasen inn i variabelen varer
     let vareList = document.getElementById('varer')
     for (let i = 0; i<varer.length; i++){
 
@@ -25,18 +25,13 @@ async function hentVarer() {
             <p> ${varer[i].kategorinavn.kategori}</p>
             </a>`;
     };
-    
-
-
-    console.log(varer)
 }
 hentVarer()
 
-function seVare(vareIndex){
+function seVare(vareIndex){ //funksjon som kjører når man trykker på en av varene
     let vareside = document.getElementById('vareside')
-    vareside.style.display = 'inline'
-    document.getElementById('main').style.opacity = '0.4'
-    console.log(varer[vareIndex])
+    vareside.style.display = 'inline' //gjør siden synlig
+    document.getElementById('main').style.opacity = '0.4' //gråer ut bakgrunnen
     vareside.innerHTML = `<div id="vareitems">
         <img id="hovedbilde" src="${bildeURL(vareIndex)}">
         <div>
---
+++ techdocs/sanity-tutorial.md
@@ -0,0 +1,21 @@
+# Hvordan bruke Åses Musikk butikk databasen!!
+
+Dette er en forklaring på hvordan man bruker Åses musikk butikk databasen.
+
+## Hvordan finner man databasen?
+For å gå inn på databasen må du besøke denne nettadressen: <https://www.placeholder.org>. Da skal du komme til en side som ser slik ut:
+
+Logg deg inn med github. Hvis du har tilgang til databasen skal du se dette:
+
+
+Hvis du ikke har tilgang til databasen må du få noen til å gi deg tilgang.
+
+
+## Hvordan legger man til en vare i databasen
+For å legge til en vare i databasen må du ha følgende informasjon om varen:
+- Navn på varen
+- Pris på varen
+- hvilken kategori varen tilhører (for eks. Plate eller CD)
+- Bilde av varen
+
+Når du har all denne informasjonen så kan du trykke på **Varer**
